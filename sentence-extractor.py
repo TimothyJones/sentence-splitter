6bf42f71d32ccd0f2bc6ff4bc3c3b69bfab9e7cc
Timothy Jones <tjones@dius.com.au>
2018-03-21 15:39:50 +1100

    Output sentences as CSV

--- sentence-extractor.py
+++ sentence-extractor.py
@@ -1,14 +1,19 @@
 import os.path
 import codecs
+import re
+import unicodecsv as csv
 from textblob import TextBlob
 
 
 inputPath = os.path.join(os.getcwd(), "input")
-listOfFiles = [os.path.join(inputPath, f) for f in os.listdir(inputPath) if os.path.isfile(os.path.join(inputPath, f))]
+outputPath = os.path.join(os.getcwd(), "output")
+listOfFiles = [os.path.join(inputPath, inputTextFile) for inputTextFile in os.listdir(inputPath) if os.path.isfile(os.path.join(inputPath, inputTextFile))]
 
-
-for f in listOfFiles:
-    print(f)
-    with codecs.open(f, 'r', 'utf-8') as content_file:
+for inputTextFile in listOfFiles:
+    print(inputTextFile)
+    with codecs.open(inputTextFile, 'r', 'utf-8') as content_file:
         text = TextBlob(content_file.read())
-        print(text.sentences)
+        sentences = [re.sub('\s+', ' ', str(s)) for s in text.sentences]
+        with open(os.path.join(outputPath, os.path.basename(inputTextFile)), 'wb') as outputCsvFile:
+            writer = csv.writer(outputCsvFile, encoding='utf-8')
+            writer.writerow(sentences)
